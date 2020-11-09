70fc34cd238f0d127d0872a587d6af5a795be9ef
stein <steinke.felix@yahoo.de>
2020-11-09 17:07:04 +0100

    added MonteCarlo

---
+++ MultiThread_MonteCarlo/src/main/java/app/Application.java
@@ -0,0 +1,21 @@
+package app;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Generator generator = new Generator();
+
+        long startTime = System.nanoTime();
+        System.out.println(generator.generatePI(100_000_000));
+        long endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
+
+        GeneratorThread generatorThread = new GeneratorThread(4);
+
+        startTime = System.nanoTime();
+        System.out.println(generatorThread.generatePi(100_000_000));
+        endTime = System.nanoTime();
+        System.out.println(endTime - startTime);
+    }
+
+}
